a47ac5fa66bbab760506f45a7e00559aaeeda854
vincent <vincent030299@hotmail.com>
2016-02-29 10:02:10 +0100

    bootstrap gelinkt

--- public/index.php
+++ public/index.php
@@ -2,6 +2,21 @@
 <html>
 <head>
 	<title>PHP Week2</title>
+	<!-- css bootstrap -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+
+	<!-- javascript bootstrap -->
+	<script type="text/javascript" href="js/bootstrap.js"></script>
+	<script type="text/javascript" href="js/bootstrap.min.js"></script>
+	<script type="text/javascript" href="js/npm.js"></script>
+
+	<!-- fonts bootstrap -->
+	<link rel="stylesheet" href="fonts/glyphicons-halflings-regular.eot">
+	<link rel="stylesheet" href="fonts/glyphicons-halflings-regular.svg">
+	<link rel="stylesheet" href="fonts/glyphicons-halflings-regular.ttf">
 </head>
 <body>
 		<h1>Het werkt</h1>
